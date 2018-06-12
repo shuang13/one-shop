4d917a0553185c4b9e77cb9a512fc539b20087b2
shuang13 <794861654@qq.com>
2018-06-13 00:05:11 +0800

    自制了一个jq弹窗插件

---
+++ src/components/popUp/popUp.js
@@ -0,0 +1,86 @@
+(function($) {
+var PopUp = function(options, callback) {
+    this.defaults = {
+        title: "提示",
+        context: "",
+        footer: '<button class="btn blue-btn comfirm-btn">确认</button><button class="btn white-btn select-btn">取消</button>',
+        // width: 250,
+        // height: 200,
+        // classname: 'selector-box',
+
+    };
+    this.template = [
+        '<div class="pop-box">',
+        '<header>',
+        '<p class="pop-title"></p>',
+        '<i class="fa fa-times" aria-hidden="true"></i>',
+        '</header>',
+        '<div class="pop-content"></div>',
+        '<footer class="pop-footer"></footer>',
+        '</div>',
+    ].join('');
+    if (callback && (typeof callback === 'function')) {
+        this.callback = callback;
+
+    }
+    this.popbox = $('<div>');
+    this.options = $.extend({}, this.defaults, options);
+}
+PopUp.prototype = {
+    init: function() {
+        this.render();
+        this.popOpen();
+
+
+
+    },
+    popOpen: function() {
+        if (this.popbox.hasClass('hidden')) {
+            this.popbox.removeClass('hidden');
+        } else {
+            return false;
+        }
+    },
+    popClose: function() {
+        this.popbox.addClass('hidden');
+    },
+    render: function() {
+        this.popbox.addClass('pop-bg');
+        this.popbox.addClass('hidden');
+        this.popbox.html(this.template);
+        $('body').append(this.popbox);
+        $('.pop-title').html(this.options.title);
+        $('.pop-content').html(this.options.context);
+        $('.pop-footer').html(this.options.footer);
+        this.bindEvent();
+    },
+    bindEvent: function() {
+        var _this = this;
+        $('.fa-times').on('click', function() {
+            _this.popClose();
+        });
+        $('.comfirm-btn').on('click', function() {
+            _this.btnComfirm();
+        });
+        $('.select-btn').on('click', function() {
+            _this.btnCancel();
+        });
+    },
+    btnComfirm: function() {
+        this.popClose();
+        this.callback();
+    },
+    btnCancel: function() {
+        this.popClose();
+    },
+
+};
+var popUp = function(options, callback) {
+    var popUp = new PopUp(options, callback);
+    popUp.init();
+    return popUp;
+}
+$.extend({
+    'popUp': popUp,
+});
+})(jQuery);
--- src/pages/cashRegister/page.js
+++ src/pages/cashRegister/page.js
@@ -1,39 +1,52 @@
 (function() {
-var aqiData = {};
 
-function addAqiData() {
+var Page = function() {
+    this.data = {};
+    this.list = [];
+}
+Page.prototype = {
+    init: function() {
+
+        var _this = this;
+        _this.setData();
+        _this.drawTable();
+
+        $('#sortTable').on('click', function(e) {
+            if (e.target.nodeName === 'BUTTON') {
+                _this.btnHandler(e.target);
+            }
+        })
+
+    },
+    btnHandler: function(ele) {
+        console.log($(ele).attr('data-id'));
+        utils.jumpUrl('../goodsDetail/index.html?coding=' + $(ele).attr('data-id'));
+    },
+    drawTable: function() {
+        var sortTable = new SortTable({
+            ele: document.getElementById('sortTable'),
+            title: ["收银时间", "收银员", "合计数量", "收银金额", "付款方式"],
+            data: this.list,
+        });
+        sortTable.init();
+        this.eventListener();
+    },
+    eventListener: function() {},
+    setData: function() {
+        var goods = state.goods.goodsList;
+        console.log(goods)
+
+        for (var i = 0; i < goods.length; i++) {
+            this.list[i] = [goods[i]['coding'],
+                goods[i]['name'],
+                '<button class="btn orange-btn" data-id=' + goods[i]['coding'] + '>查看详情</button>'
+            ];
+        }
+    },
 
 }
-
-/**
- * 渲染aqi-table表格
- */
-function renderAqiList() {
-
-}
-
-/**
- * 点击add-btn时的处理逻辑
- * 获取用户输入，更新数据，并进行页面呈现的更新
- */
-function addBtnHandle() {
-    addAqiData();
-    renderAqiList();
-}
-
-/**
- * 点击各个删除按钮的时候的处理逻辑
- * 删除数据，更新表格显示
- */
-function delBtnHandle(city) {
-}
-
-function init() {
-
-    // add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-    // aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
-}
-
-init();
-})()
+var page = new Page();
+setTimeout(function() {
+    page.init();
+}, 500);
+})();
