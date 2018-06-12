73a54b043800cd0e36c2a5a9d79e243ac45397f4
Zhang-feilong <zhang.feilong1@foxmail.com>
2018-06-12 22:28:54 +0800

    头部，侧栏及底部的初步完成

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -5,28 +5,14 @@ $(document).ready(function(){
 	$(".person").mouseout(function(){
 		$(".change-acount").css("display","none");
 	});
-	$(".sub").click(function(){
-		$(".sub-menu-item").toggle(500);
+	var r = 0;
+	$(".sub-menu").click(function(){
+		$(".sub-menu-item").slideToggle(function () {
+			console.log(r);
+			r += 180;
+			$(".jiantou1").css("transform","rotate("+r+"deg)");	
+		});
+		
 	});
-	$(".menu-item").click(function(){
-		$(this).css({
-			"color": "#2d8cf0",
-   			"border-right": "2px solid #2d8cf0"
-		}).siblings().css({
-			"color": "rgba(255, 255, 255, 0.7)",
-   			"border-right": "none"
-		})
-	});
-	$(".sub-item").click(function(){
-		$(this).css({
-    		"color": "#fff",
-    		"background": "#2d8cf0",
-    		"border-right": "none"
-		}).siblings().css({
-			"color": "rgba(255, 255, 255, 0.7)",
-   			"background": "#363e4f",
-   			"border-right": "none"
-		})
-	})
 	
 });
